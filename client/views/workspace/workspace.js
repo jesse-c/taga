9cddd2c29c57a0e478cad9b1e50851520d913694
Jesse Claven <jesse.claven@gmail.com>
2015-05-20 16:59:22 +1000

    Use schemas and helpers on collections

---
+++ client/views/workspace/workspace.js
@@ -0,0 +1,16 @@
+Template.workspace.helpers({
+  /*
+  'workspace': function() {
+    return Workspaces.findOne(?);
+  }
+  */
+  /*
+  'room': function() {
+    console.log(Rooms.findOne());
+    return Rooms.findOne();
+  }
+  */
+});
+
+Template.workspace.rendered = function() {
+};
--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,15 @@
 Meteor.publish('workspaces', function() {
-  return Workspaces.find()
+  return Workspaces.find();
+});
+
+Meteor.publish('workspace', function(workspaceId) {
+  return Workspaces.find(workspaceId);
 });
 
 Meteor.publish('rooms', function() {
-  return Roomss.find()
+  return Rooms.find();
+});
+
+Meteor.publish('room', function(roomId) {
+  return Rooms.find(roomId);
 });
