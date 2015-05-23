b0a1977b0e8458af7f98b9470acbce7f295bd7b0
Jesse Claven <jesse.claven@gmail.com>
2015-05-24 00:12:59 +1000

    Add starting rooms Add hasSpace function

--- client/views/index/index.js
+++ client/views/index/index.js
@@ -1,2 +1,27 @@
 Template.index.helpers({
 });
+
+Template.index.events({
+  'click .workspace__form-start': function(e) {
+    // Get name
+    var name = $('#workspace__form-name').val();
+
+    //
+    var values = {
+      _workspace: $('#workspace__form-workspace option:selected').val(),
+      _owner: Meteor.userId(),
+      name: name,
+      _users: [],
+      _chat: name
+    };
+    console.log(values);
+
+    // TODO
+    //Streamy.join(name);
+
+    var id = Rooms.insert(values);
+
+    // 
+    Router.go('room', { _id: id });
+  }
+});
