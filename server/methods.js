bf3be5f47f66a13429f9a92b722ae20f850b5b36
Jesse Claven <jesse.claven@gmail.com>
2015-05-26 21:52:20 +1000

    Add position saving

--- server/methods.js
+++ server/methods.js
@@ -4,5 +4,30 @@ Meteor.methods({
   },
   'endRoom': function(id) {
     Rooms.remove(id);
+  },
+  'lock': function(id) {
+    ObjectInstances.update(id, { 
+      $set: { 
+        _controller: Meteor.userId()
+      }
+    });
+  },
+  'updatePosition': function(id, top, left) {
+    ObjectInstances.update(id, { 
+      $set: { 
+        _controller: Meteor.userId(),
+        top: top,
+        left: left
+      }
+    });
+  },
+  'unlock': function(id, top, left) {
+    ObjectInstances.update(id, { 
+      $set: { 
+        _controller: '',
+        top: top,
+        left: left
+      }
+    });
   }
 });
