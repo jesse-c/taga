5465be599494ffaad87a910c99db2e7714e22fec
Jesse Claven <jesse.claven@gmail.com>
2015-05-29 11:21:01 +1000

    Correct updating Sensor values and convert to range 0..255

--- server/methods.js
+++ server/methods.js
@@ -6,9 +6,17 @@ Meteor.methods({
     Rooms.update(Rooms.findOne()._id, { $set: { status: newStatus }});
   },
   'readSensor': function(r, g, b) {
+    function convertRange( value, r1, r2 ) { 
+          return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
+    }
+
+    r = Math.floor(convertRange(r, [0, 5000], [0, 255]));
+    g = Math.floor(convertRange(g, [0, 5000], [0, 255]));
+    b = Math.floor(convertRange(b, [0, 5000], [0, 255]));
+
     console.log('Called readSensor');
     console.log(r, g, b);
-    Sensor.update(Sensor.find()._id, {
+    Sensor.update(Sensor.findOne(), {
       $set: {
         r: r,
         g: g,
